04c791e979637ce5b0dbea3c1d43b76dbf8bd6f2
Tim Mackay <simplytim42@gmail.com>
2023-10-04 20:32:02 +0100

    initial code

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from pathlib import Path
+from datetime import date
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+
+load_dotenv()
+
+file_db = Path("products.txt")
+date_today = date.today().isoformat()
+
+response = requests.get(os.getenv("PRODUCT_URL"))
+soup = BeautifulSoup(response.text, "html.parser")
+product_title = soup.find("span", "product-name").string
+price = soup.find("span", "regular-price").string
+
+with file_db.open("a") as f:
+    f.write(f"{date_today}|{product_title}|{price}\n")
+
+
+NOTIFICATION_URL = "https://api.pushover.net/1/messages.json"
+response = requests.post(
+    NOTIFICATION_URL,
+    data={
+        "token": os.getenv("NOTIFICATION_TOKEN"),
+        "user": os.getenv("NOTIFICATION_USER_KEY"),
+        "title": product_title,
+        "message": price,
+        "sound": "pianobar",
+    },
+)
