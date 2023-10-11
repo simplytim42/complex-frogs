a09a1fc96b1619f9a16e3be3f25a40c7bd31eaaa
Tim Mackay <simplytim42@gmail.com>
2023-10-11 18:52:19 +0100

    update scraper to use httpx and selectolax

--- tools/scraper.py
+++ tools/scraper.py
@@ -1,12 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+import httpx
+from selectolax.parser import HTMLParser
 
 
 class Scraper:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+    }
+
     def __init__(self, url: str):
         self.url = url
 
     def get_value(self, css_selector: str) -> str:
-        response = requests.get(self.url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        return soup.css.select(css_selector)[0].string.strip()
+        response = httpx.get(self.url, headers=self.headers)
+        html = HTMLParser(response.text)
+        return html.css_first(css_selector).text()
