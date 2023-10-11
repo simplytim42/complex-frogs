82cd34c8ea7d21719a67f643e527d866342870b1
Tim Mackay <simplytim42@gmail.com>
2023-10-11 18:51:16 +0100

    db checks if record exists before adding

--- tools/db.py
+++ tools/db.py
@@ -2,13 +2,20 @@ from pathlib import Path
 from datetime import date
 
 
-class FileDatabase:
+class Database:
     def __init__(self):
         self.db = Path("products.txt")
         if not self.db.exists():
             self.db.touch()
 
-    def write_line(self, *args):
-        args = date.today().isoformat(), *args
-        with self.db.open("a") as f:
-            f.write("|".join(args) + "\n")
+    def add_record(self, *args):
+        today = date.today().isoformat()
+        values = today, *args
+        record = "|".join(values) + "\n"
+
+        if not self._record_exists(record):
+            with self.db.open("a") as f:
+                f.write(record)
+
+    def _record_exists(self, record):
+        return record in self.db.read_text()
