0f77752278f3b8ebfa29aaefa5a6572ac208b9b2
Tim Mackay <simplytim42@gmail.com>
2023-10-11 18:52:42 +0100

    update to use new changes to tool classes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 from tools.notifications import PushNotification
-from tools.db import FileDatabase
+from tools.db import Database
 from tools.scraper import Scraper
 
 notification = PushNotification()
-db = FileDatabase()
+db = Database()
 
 
 with open("data/scraping_data.json") as f:
@@ -14,7 +14,9 @@ for product in products:
     scraper = Scraper(product["url"])
     price = scraper.get_value(product["css_selector"])
 
-    db.write_line(product["title"], price)
+    db.add_record(product["title"], price)
 
     if product["notification"]:
         notification.send(product["title"], price)
+
+    print(f"Added '{product['title']}' with price {price}")
