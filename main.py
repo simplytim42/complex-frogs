bf297469876e672a7ab0fe22a3454f9f10efc379
Tim Mackay <simplytim42@gmail.com>
2023-10-12 14:20:13 +0100

    update to use py-pushover-client package

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import json
 import time
-from tools.notifications import PushNotification
+import os
+from py_pushover_client import PushoverAPIClient
 from tools.db import Database
 from tools.scraper import Scraper
+from dotenv import load_dotenv
 
-notification = PushNotification()
+
+load_dotenv()
+API_TOKEN = os.getenv("NOTIFICATION_TOKEN")
+USER_KEY = os.getenv("NOTIFICATION_USER_KEY")
+notification = PushoverAPIClient(api_token=API_TOKEN, user_key=USER_KEY)
 db = Database()
 
 
 with open("data/scraping_data.json") as f:
     products = json.load(f)
+
 
 for product in products:
     scraper = Scraper(product["url"])
@@ -18,8 +25,9 @@ for product in products:
     db.add_record(product["title"], price)
 
     if product["notification"]:
-        notification.send(product["title"], price)
+        notification.send(title=product["title"], message=price)
 
     print(f"Added '{product['title']}' with price {price}")
+
     # Sleep for 5 seconds to avoid getting blocked
     time.sleep(5)
