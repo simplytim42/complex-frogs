f8107f4394f1a3d9016a6517b51a8fe1a32cad5e
Tim Mackay <simplytim42@gmail.com>
2023-10-17 13:16:09 +0100

    raise error when incorrect site id

--- tools/scraper_dispatcher.py
+++ tools/scraper_dispatcher.py
@@ -3,6 +3,9 @@ from .go_od_scraper import GoOutdoorsScraper
 
 
 def get_scraper(site: str, product_id: str):
+    if site not in ["amz", "go_od"]:
+        raise ValueError(f"Invalid site: {site}")
+
     if site == "amz":
         return AmazonScraper(product_id)
     if site == "go_od":
