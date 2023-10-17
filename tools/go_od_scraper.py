646af62ad13b3d60c686d73bb6e240918bc262d8
Tim Mackay <simplytim42@gmail.com>
2023-10-17 13:16:31 +0100

    add logging and refactor

--- tools/go_od_scraper.py
+++ tools/go_od_scraper.py
@@ -1,38 +1,41 @@
 import httpx
+import logging
 from selectolax.parser import HTMLParser
 from .base_scraper import BaseScraper, ScraperException
 
 
 class GoOutdoorsScraper(BaseScraper):
     html = None
+    PRICE_SELECTOR = "span.regular-price"
+    TITLE_SELECTOR = "span.product-name"
 
     def __init__(self, id: str):
         sku = id.split("-")[-1]
-        self.url = f"https://www.gooutdoors.co.uk/{sku}/{id}"
+        self.URL = f"https://www.gooutdoors.co.uk/{sku}/{id}"
 
     def __get_html(self):
         try:
-            response = httpx.get(self.url, headers=self.headers)
+            response = httpx.get(self.URL, headers=self.headers)
             response.raise_for_status()
             self.html = HTMLParser(response.text)
         except Exception as e:
-            print(e)
+            logging.error(f"Error getting HTML: {e}")
             raise ScraperException("Failed to get HTML")
 
     def get_price(self) -> str:
         if not self.html:
             self.__get_html()
         try:
-            return self.html.css_first("span.regular-price").text(strip=True)
+            return self.html.css_first(self.PRICE_SELECTOR).text(strip=True)
         except AttributeError as e:
-            print(e)
+            logging.warning(f"Error getting price: {e}")
             return "Price not found"
 
     def get_title(self) -> str:
         if not self.html:
             self.__get_html()
         try:
-            return self.html.css_first("span.product-name").text(strip=True)
+            return self.html.css_first(self.TITLE_SELECTOR).text(strip=True)
         except AttributeError as e:
-            print(e)
+            logging.warning(f"Error getting title: {e}")
             return "Title not found"
