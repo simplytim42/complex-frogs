4e77cf6d477a15105a0dd594a73b2bc54a135657
Tim Mackay <simplytim42@gmail.com>
2023-10-17 13:15:34 +0100

    add logging to db

--- tools/db.py
+++ tools/db.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import date
+import logging
 
 
 class Database:
@@ -9,15 +10,18 @@ class Database:
         self.db = Path(__file__).resolve().parent.parent / self.db_name
         if not self.db.exists():
             self.db.touch()
+            logging.debug(f"Created database {self.db_name}")
 
     def add_record(self, *args):
         today = date.today().isoformat()
         values = today, *args
         record = "|".join(values) + "\n"
+        logging.debug(f"Preparing to add record: |||{record.strip()}|||")
 
         if not self._record_exists(record):
             with self.db.open("a") as f:
                 f.write(record)
+                logging.info(f"Added record: |||{record.strip()}|||")
 
     def _record_exists(self, record):
         return record in self.db.read_text()
