d5bffc2de41f7634e0d6074053fd6c4999ba817d
Tim Mackay <simplytim42@gmail.com>
2023-10-19 09:14:46 +0100

    add workaround scraper for amz products

---
+++ tools/amazon_google_scraper.py
@@ -0,0 +1,66 @@
+from playwright.sync_api import sync_playwright
+from selectolax.parser import HTMLParser
+from .base_scraper import BaseScraper, ScraperException
+import logging
+
+
+class AmazonGoogleScraper(BaseScraper):
+    PRICE_SELECTOR = "span.T14wmb"
+    TITLE_SELECTOR = "h3.sh-np__product-title"
+    REJECT_COOKIES_SELECTOR = "div.VfPpkd-RLmnJb"
+    PRODUCT_CARDS_SELECTOR = "div.KZmu8e"
+    PRODUCT_DETAILS_SELECTOR = "div.HUOptb"
+    html = None
+    node = None
+
+    def __init__(self, query: str):
+        query = query.replace(" ", "+")
+        self.URL = f"https://www.google.com/search?q={query}&tbm=shop"
+
+    def __retrieve_html(self):
+        try:
+            pw = sync_playwright().start()
+            browser = pw.chromium.launch()
+            context = browser.new_context(extra_http_headers=self.HEADERS)
+            page = context.new_page()
+            page.goto(self.URL)
+            page.click(self.REJECT_COOKIES_SELECTOR)
+            page.wait_for_load_state("networkidle")
+            self.html = HTMLParser(page.content())
+
+            nodes = self.html.css(self.PRODUCT_CARDS_SELECTOR)
+            for node in nodes:
+                if node.select(self.PRODUCT_DETAILS_SELECTOR).any_text_contains(
+                    "Amazon.co.uk"
+                ):
+                    self.node = node
+                    break
+        except Exception as e:
+            logging.error(f"Error getting HTML: {e}")
+            raise ScraperException("Failed to get HTML")
+        finally:
+            browser.close()
+            pw.stop()
+
+    def get_html(self) -> str:
+        if not self.html:
+            self.__retrieve_html()
+        return self.html.html
+
+    def get_price(self) -> str:
+        if not self.node:
+            self.__retrieve_html()
+        try:
+            return self.node.css_first(self.PRICE_SELECTOR).text(strip=True)
+        except AttributeError as e:
+            logging.warning(f"Error getting price: {e}")
+            return self.PRICE_404
+
+    def get_title(self) -> str:
+        if not self.node:
+            self.__retrieve_html()
+        try:
+            return self.node.css_first(self.TITLE_SELECTOR).text(strip=True)
+        except AttributeError as e:
+            logging.warning(f"Error getting title: {e}")
+            return self.TITLE_404
--- tools/scraper_dispatcher.py
+++ tools/scraper_dispatcher.py
@@ -1,12 +1,15 @@
 from .amazon_scraper import AmazonScraper
 from .go_od_scraper import GoOutdoorsScraper
+from .amazon_google_scraper import AmazonGoogleScraper
 
 
 def get_scraper(site: str, product_id: str):
-    if site not in ["amz", "go_od"]:
+    if site not in ["amz", "amz-g", "go_od"]:
         raise ValueError(f"Invalid site: {site}")
 
     if site == "amz":
         return AmazonScraper(product_id)
+    if site == "amz-g":
+        return AmazonGoogleScraper(product_id)
     if site == "go_od":
         return GoOutdoorsScraper(product_id)
