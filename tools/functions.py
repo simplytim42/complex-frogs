ecfdfbb6c35b7c7d1f55704cd56f5cc8dd922673
Tim Mackay <simplytim42@gmail.com>
2023-11-14 20:08:27 +0000

    adding type of Path to write_file

--- tools/functions.py
+++ tools/functions.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from pathlib import Path
 
 
-def write_file(dir: str, filename: str, content: str) -> Path:
+def write_file(dir: Path, filename: str, content: str) -> Path:
     """Write a file to a directory and return the path to the file."""
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    filepath = Path(dir) / f"{timestamp}_{filename}"
+    filepath = dir / f"{timestamp}_{filename}"
     with Path(__file__).resolve().parent / filepath as file:
         file.parent.mkdir(parents=True, exist_ok=True)
         file.write_text(content)
