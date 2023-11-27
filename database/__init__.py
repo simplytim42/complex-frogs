2744ae841ec97ce19f63852cb75c16dbc57037f6
Tim Mackay <simplytim42@gmail.com>
2023-11-27 16:22:50 +0000

    be explicit with args

--- database/__init__.py
+++ database/__init__.py
@@ -5,4 +5,4 @@ from .models import Base
 db_location = Path(__file__).resolve().parent.parent / "frog.db"
 engine = create_engine(f"sqlite:////{db_location}")
 
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine, checkfirst=True)
