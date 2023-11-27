051dfb4cde15b12cd4ac39636d612256e7610d8c
Tim Mackay <simplytim42@gmail.com>
2023-11-27 16:20:26 +0000

    emit table creation if tables dont exist

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
 from pathlib import Path
+from .models import Base
 
 db_location = Path(__file__).resolve().parent.parent / "frog.db"
 engine = create_engine(f"sqlite:////{db_location}")
+
+Base.metadata.create_all(engine)
