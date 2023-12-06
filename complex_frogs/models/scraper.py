235ff1406fd02bb168c72dc51da321194e303af5
Tim Mackay <simplytim42@gmail.com>
2023-12-06 22:02:34 +0000

    prevent ruff from flagging the id field

--- complex_frogs/models/scraper.py
+++ complex_frogs/models/scraper.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class Target(BaseModel):
     """model for a scraping target."""
 
-    id: int
+    id: int  # noqa: A003
     site: str
     sku: str
     send_notification: bool
