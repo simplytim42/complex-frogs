e2af4adc7bb720d53256c1dde1dc5dbac7c2da4e
Tim Mackay <simplytim42@gmail.com>
2023-12-06 17:39:17 +0000

    remove logging in prep for using the new logging function

--- run_api.py
+++ run_api.py
@@ -1,7 +1,6 @@
 """Main API file for Complex Frogs API."""
 
 import logging
-from pathlib import Path
 
 from fastapi import FastAPI, HTTPException
 from sqlalchemy import select
@@ -12,14 +11,6 @@ from complex_frogs.database import engine
 from complex_frogs.database.models import ScrapeTargets
 from complex_frogs.models.scraper import Target
 
-LOGS_DIR = Path(__file__).parent / "logs"
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    filename=LOGS_DIR / "api.log",
-)
-
 app = FastAPI()
 session = Session(engine)
 
@@ -27,6 +18,7 @@ session = Session(engine)
 @app.get("/")
 def root():
     """Root endpoint for API."""
+    logging.info("Root endpoint hit")
     return {"message": "Complex Frogs API"}
 
 
@@ -34,6 +26,7 @@ def root():
 def get_targets():
     """Get all scraping targets from database."""
     try:
+        logging.info("Getting targets from database")
         stmt = select(ScrapeTargets)
         targets = session.scalars(stmt)
     except SQLAlchemyError as e:
