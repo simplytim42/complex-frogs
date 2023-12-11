4f7939d9c1b0e155e18fece2713a66869c2d33ec
Tim Mackay <simplytim42@gmail.com>
2023-12-11 17:11:20 +0000

    improve typing

--- complex_frogs/database/crud.py
+++ complex_frogs/database/crud.py
@@ -1,18 +1,20 @@
 """Create Read Update & Delete operations for the database."""
 
+from typing import Sequence
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from .models import ScrapeTargets
 
 
-def read_targets(session: Session):
+def read_targets(session: Session) -> Sequence[ScrapeTargets]:
     """Get all scraping targets from database."""
     stmt = select(ScrapeTargets)
-    return session.scalars(stmt)
+    return session.scalars(stmt).all()
 
 
-def read_target(session: Session, target_id: int):
+def read_target(session: Session, target_id: int) -> ScrapeTargets | None:
     """Get a single scraping target from database."""
     stmt = select(ScrapeTargets).where(ScrapeTargets.id == target_id)
     return session.scalar(stmt)
