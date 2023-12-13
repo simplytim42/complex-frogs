322231331c33d5e0caf7211a4c5614882524ac0e
Tim Mackay <simplytim42@gmail.com>
2023-12-13 15:36:54 +0000

    improved model tests and reprs

--- tests/database/test_models.py
+++ tests/database/test_models.py
@@ -8,6 +8,32 @@ from complex_frogs.database.models import Base, ScrapedData, ScrapeTargets
 
 
 @pytest.fixture()
+def timestamp():
+    return datetime.now(tz=timezone.utc)
+
+
+@pytest.fixture()
+def target(timestamp):
+    return ScrapeTargets(
+        site="test site",
+        sku="test sku",
+        send_notification=True,
+        date_added=timestamp,
+        last_scraped=timestamp,
+    )
+
+
+@pytest.fixture()
+def scraped_data(timestamp):
+    return ScrapedData(
+        scrape_target_id=1,
+        title="test title",
+        price="£10",
+        timestamp=timestamp,
+    )
+
+
+@pytest.fixture()
 def session():
     engine = create_engine("sqlite:///:memory:")
     session = Session(engine)
@@ -16,15 +42,14 @@ def session():
     Base.metadata.drop_all(engine)
 
 
-def test_create_scrape_target(session):
-    now = datetime.now(tz=timezone.utc)
-    target = ScrapeTargets(
-        site="test site",
-        sku="test sku",
-        send_notification=True,
-        date_added=now,
-        last_scraped=now,
+def test_scrape_target_repr(target):
+    expected_repr = (
+        "ScrapeTargets(site='test site', sku='test sku', send_notification=True)"
     )
+    assert repr(target) == expected_repr
+
+
+def test_create_scrape_target(session, target, timestamp):
     session.add(target)
     session.commit()
 
@@ -43,29 +68,20 @@ def test_create_scrape_target(session):
     assert retrieved_target.sku == "test sku"
     assert retrieved_target.send_notification is True
     # testing datetimes as aware to ensure they can be entereted into the db as naive
-    assert retrieved_target.date_added == now
-    assert retrieved_target.last_scraped == now
+    assert retrieved_target.date_added == timestamp
+    assert retrieved_target.last_scraped == timestamp
 
 
-def test_create_scrape_data(session):
-    now = datetime.now(tz=timezone.utc)
-    target = ScrapeTargets(
-        site="test site",
-        sku="test sku",
-        send_notification=True,
-        date_added=now,
-        last_scraped=now,
-    )
+def test_scrape_data_repr(scraped_data):
+    expected_repr = "ScrapedData(scrape_target_id=1, title='test title', price='£10')"
+    assert repr(scraped_data) == expected_repr
+
+
+def test_create_scrape_data(session, target, scraped_data, timestamp):
     session.add(target)
     session.commit()
 
-    data = ScrapedData(
-        scrape_target_id=target.id,
-        title="test title",
-        price="test price",
-        timestamp=now,
-    )
-    session.add(data)
+    session.add(scraped_data)
     session.commit()
 
     stmt = select(ScrapedData)
@@ -78,5 +94,5 @@ def test_create_scrape_data(session):
 
     assert retrieved_data.scrape_target_id == target.id
     assert retrieved_data.title == "test title"
-    assert retrieved_data.price == "test price"
-    assert retrieved_data.timestamp == now
+    assert retrieved_data.price == "£10"
+    assert retrieved_data.timestamp == timestamp
