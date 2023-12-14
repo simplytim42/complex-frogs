0aa0a87d4a868f137c6380e8168fc827b227a2b2
Tim Mackay <simplytim42@gmail.com>
2023-12-14 14:08:10 +0000

    Add root API endpoint

--- run_api.py
+++ run_api.py
@@ -1,8 +1,8 @@
 """Main API file for Complex Frogs API."""
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI
 
-from complex_frogs.api import scrape_data, targets
+from complex_frogs.api import root, scrape_data, targets
 from complex_frogs.logger.config import LOGS_DIR, setup_logger
 
 setup_logger(LOGS_DIR / "api.log")
@@ -10,13 +10,4 @@ setup_logger(LOGS_DIR / "api.log")
 app = FastAPI()
 app.include_router(targets.router)
 app.include_router(scrape_data.router)
-
-
-@app.get(
-    "/",
-    status_code=status.HTTP_418_IM_A_TEAPOT,
-    response_description="Generic Message. Used to confirm API is running.",
-)
-def root() -> dict[str, str]:
-    """Root endpoint for API."""
-    return {"message": "Complex Frogs API"}
+app.include_router(root.router)
